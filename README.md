52207eda2d719eb3ab650f084b8c921e2cbef6d5
coderwangson <572043wyh@outlook.com>
2019-02-10 10:39:10 +0800

    update

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 flappy代码引用：https://github.com/sourabhv/FlapPyBird  
 
-其他代码都是手写
+其他代码都是手写 
+
+如何运行  
+
+1、你可以直接运行flappy.py但是可能模型不好，所以不灵敏  
+
+2、你可以自己生成自己的模型  
+
+2.1 先运行collect_data进行图片收集  
+
+2.2 然后运行process_data生成两个pkl文件，并且get_batch能够得到数据 
+
+2.3 运行Model生成模型  
+
+2.4 把predict下面代码注释打开，测试一下是否模型好用，如果好用则注释掉 
+
+2.5 运行flappy
--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 """
 Created on 2019/2/9 19:16
-
+@File:predict.py
 @author: coderwangson
 """
 "#codeing=utf-8"
@@ -12,29 +12,29 @@ def predict(img,sess,x_image,y_,keep_drop):
     y = sess.run(y_, feed_dict={x_image:img, keep_drop: 1.0})
     return np.argmax(y)
 # 因为sess加载耗时间，所以在程序中使用，这样我们就能避免每次加载耗时间
-start = time.clock()
-with tf.Session() as sess:
-    saver = tf.train.import_meta_graph("./model/hand.meta")
-    saver.restore(sess,tf.train.latest_checkpoint("./model"))
-    graph = tf.get_default_graph()
-    x_image = graph.get_tensor_by_name("Placeholder:0")
-    y_ = graph.get_tensor_by_name("add_3:0")
-    keep_drop = graph.get_tensor_by_name("Placeholder_2:0")
-    # print(time.clock() - start)
-    capture = cv.VideoCapture(0)
-    while True:
-        ha, img = capture.read()
-        img = cv.flip(img,1)
-        cv.rectangle(img, (426, 50), (640, 350), (170, 170, 0))
-        img = img[50:350, 426:640, :]
-        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        cv.imshow("img", img)
-        img = cv.resize(img, (64, 64)).reshape((1, 64, 64, 1))
-
-        # 0是拳头 1 是手掌
-        action = predict(img, sess,x_image,y_,keep_drop)
-        if action ==1:
-            print("张开")
-        elif action==0:
-            print("合住")
-        cv.waitKey(24)
+# start = time.clock()
+# with tf.Session() as sess:
+#     saver = tf.train.import_meta_graph("./model/hand.meta")
+#     saver.restore(sess,tf.train.latest_checkpoint("./model"))
+#     graph = tf.get_default_graph()
+#     x_image = graph.get_tensor_by_name("Placeholder:0")
+#     y_ = graph.get_tensor_by_name("add_3:0")
+#     keep_drop = graph.get_tensor_by_name("Placeholder_2:0")
+#     # print(time.clock() - start)
+#     capture = cv.VideoCapture(0)
+#     while True:
+#         ha, img = capture.read()
+#         img = cv.flip(img,1)
+#         cv.rectangle(img, (426, 50), (640, 350), (170, 170, 0))
+#         img = img[50:350, 426:640, :]
+#         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#         cv.imshow("img", img)
+#         img = cv.resize(img, (64, 64)).reshape((1, 64, 64, 1))
+#
+#         # 0是拳头 1 是手掌
+#         action = predict(img, sess,x_image,y_,keep_drop)
+#         if action ==1:
+#             print("张开")
+#         elif action==0:
+#             print("合住")
+#         cv.waitKey(24)
